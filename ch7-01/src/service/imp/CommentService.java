c45a7bb65553cf2c7bc83f10168cf87b74bb8bc0
GawainChen <www.986788924@qq.com>
2016-07-08 00:19:13 +0800

    Gawain_Chen
    完成了业务逻辑层编写

--- ch7-01/src/service/imp/CommentService.java
+++ ch7-01/src/service/imp/CommentService.java
@@ -2,39 +2,64 @@ package service.imp;
 
 import java.util.List;
 
+import dao.ICommentDAO;
 import domain.Comment;
 import service.ICommentService;
 
 public class CommentService implements ICommentService {
+	/*
+	 * 定义ICommentDAO类型的属性，以实现CommentService和ICommentDAO实现类之间的协作
+	 */
+	private ICommentDAO commentDAO;
+
+	public CommentService() {
+
+	}
+
+	public void setCommentDAO(ICommentDAO commentDAO) {
+		this.commentDAO = commentDAO;
+	}
 
 	@Override
 	public void addComment(Comment comment) {
-		// TODO 自动生成的方法存根
+		if (comment == null)
+			return;
+		if (comment.getId() != null)
+			return;
+		commentDAO.addComment(comment);
 
 	}
 
 	@Override
 	public void deleteComment(Comment comment) {
-		// TODO 自动生成的方法存根
+		if (comment == null)
+			return;
+		if (comment.getId() == null)
+			return;
+		commentDAO.deleteComment(comment);
 
 	}
 
 	@Override
 	public void updateComment(Comment comment) {
-		// TODO 自动生成的方法存根
+		if (comment == null)
+			return;
+		if (comment.getId() == null)
+			return;
+		commentDAO.updateComment(comment);
 
 	}
 
 	@Override
 	public Comment getCommentByID(String id) {
-		// TODO 自动生成的方法存根
-		return null;
+		if (id == null)
+			return null;
+		return commentDAO.getCommentById(id);
 	}
 
 	@Override
 	public List<Comment> getAllComment() {
-		// TODO 自动生成的方法存根
-		return null;
+		return commentDAO.getAllComments();
 	}
 
 }
--- ch7-01/src/service/imp/InfoService.java
+++ ch7-01/src/service/imp/InfoService.java
@@ -2,39 +2,68 @@ package service.imp;
 
 import java.util.List;
 
+import dao.IInfoDAO;
 import domain.Information;
 import service.IInfoService;
 
 public class InfoService implements IInfoService {
+	/*
+	 * 定义IInfoDAO类型的属性，以实现InfoService和IInfoDAO实现类之间的协作
+	 */
+	private IInfoDAO infoDAO;
+
+	public InfoService() {
+
+	}
+
+	public InfoService(IInfoDAO infoDAO) {
+		this.infoDAO = infoDAO;
+	}
+
+	public void setInfoDAO(IInfoDAO infoDAO) {
+		this.infoDAO = infoDAO;
+	}
 
 	@Override
 	public void addInfo(Information information) {
-		// TODO 自动生成的方法存根
+		if (information == null)
+			return;
+		if (information.getId() != null)
+			return;
+		infoDAO.addInfo(information);
 
 	}
 
 	@Override
 	public void deleteInfo(Information information) {
-		// TODO 自动生成的方法存根
+		if (information == null)
+			return;
+		if (information.getId() == null)
+			return;
+		infoDAO.deleteInfo(information);
 
 	}
 
 	@Override
 	public void updateInfo(Information information) {
-		// TODO 自动生成的方法存根
+		if (information == null)
+			return;
+		if (information == null)
+			return;
+		infoDAO.updateInfo(information);
 
 	}
 
 	@Override
 	public Information getInfoByID(String id) {
-		// TODO 自动生成的方法存根
-		return null;
+		if (id == null)
+			return null;
+		return infoDAO.getInfoByID(id);
 	}
 
 	@Override
 	public List<Information> getAllInfo() {
-		// TODO 自动生成的方法存根
-		return null;
+		return infoDAO.getAllInfo();
 	}
 
 }
